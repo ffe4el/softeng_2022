dfee1a1f109fd5ccb19fed00386d9a82d9391b53
SOLA <93892724+ffe4el@users.noreply.github.com>
2022-10-25 18:18:38 +0900

    지금 장고 중간 프젝중

--- todo_app/views.py
+++ todo_app/views.py
@@ -3,9 +3,11 @@ from django.views.generic import (
     ListView,
     CreateView,
     UpdateView,
+    DeleteView,
 )
 from .models import ToDoList
 from .models import ToDoList, ToDoItem
+from django.urls import reverse, reverse_lazy
 
 class ListListView(ListView):
     model = ToDoList
@@ -76,3 +78,21 @@ class ItemUpdate(UpdateView):
 
     def get_success_url(self):
         return reverse("list", args=[self.object.todo_list_id])
+
+
+class ListDelete(DeleteView):
+    model = ToDoList
+    # You have to use reverse_lazy() instead of reverse(),
+    # as the urls are not loaded when the file is imported.
+    success_url = reverse_lazy("index")
+
+class ItemDelete(DeleteView):
+    model = ToDoItem
+
+    def get_success_url(self):
+        return reverse_lazy("list", args=[self.kwargs["list_id"]])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = self.object.todo_list
+        return context
